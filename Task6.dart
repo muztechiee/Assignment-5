3e17e4f1ecf28a337b03b132ac00e03d98021a75
muztechiee <muztechiee@gmail.com>
2025-11-23 09:30:55 +0500

    Task6

---
+++ Task6.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<int> numbers = [2, 5, 7, 1, 9, 4, 6];
+
+  for (int number in numbers) {
+    if (number > 5) {
+      print('number is greater than 5');
+    } else {
+      print('number is not greater than 5');
+    }
+  }
+}
